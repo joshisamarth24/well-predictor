0445f545ed5f3882b909f1ff3a611a691bb0baac
tushar45264 <tushar.21ug2028@iiitranchi.ac.in>
2023-09-11 18:54:17 +0530

    slider

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,21 @@
 import React from 'react'
 import Header from './components/Header/Header';
+import { Box } from '@mui/material';
+import Home from './components/Home/Home';
+import Contact from './components/Home/Contact';
+import {Route, BrowserRouter,Routes} from 'react-router-dom'
 const App = () => {
   return (
-    <Header />
+    <>  <BrowserRouter>
+       <Header />
+       <Box sx={{marginTop:'30px'}}>
+       <Routes>
+       <Route path="/" element={<Home />} />
+       <Route path="/contact" element={<Contact />} />
+       </Routes>
+       </Box>
+        </BrowserRouter>
+    </>
   )
 }
 
---
+++ client/src/components/Home/Slide.jsx
@@ -0,0 +1,56 @@
+import { Box,styled } from '@mui/material';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+import s1 from '../../assets/1b.png'
+import s2 from '../../assets/2b.png'
+import s3 from '../../assets/4b.png'
+
+const responsive = {
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 1
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 1
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1
+    }
+  };
+
+const Slide = () => {
+  return (
+    <Component>
+                <Carousel responsive={responsive}
+              containerClass="carousel-container"
+              dotListClass="custom-dot-list-style"
+              itemClass="carousel-item-padding-40-px"
+              swipeable={false}
+              draggable={false}
+              infinite={true}
+              autoPlay={true}
+              autoPlaySpeed={4000}
+    >
+      
+        <Image src={s1} alt="banner"/>
+        <Image src={s2} alt="banner"/>
+        <Image src={s3} alt="banner"/>
+      
+    </Carousel>
+
+    </Component>
+  )
+}
+
+const Component = styled(Box)`
+    margin-top: 10px;
+    background: #fff;
+`;
+
+const Image =styled('img')({
+    width:'100%',
+    height:280,
+})
+export default Slide
