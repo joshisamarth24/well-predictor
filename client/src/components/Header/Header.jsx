5aadd3e63e51494b5f68cbafaf3dcc0825d84ef8
tushar45264 <tushar.21ug2028@iiitranchi.ac.in>
2023-09-11 14:09:48 +0530

    nav-done

---
+++ client/src/components/Header/Header.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Box,AppBar,Toolbar,Typography,Button } from '@mui/material'
+import FlexBetween from '../../requiredComponents/flexBetween'
+import styled from '@emotion/styled';
+
+const Header = () => {
+  return (
+    <container sx={{ flexGrow: 1 }}>
+      <AppBar position="fixed">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Well Predictor
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </container>
+  )
+}
+
+
+const container = styled(Box)`
+    margin:0;
+    padding:0;
+`;
+
+export default Header
